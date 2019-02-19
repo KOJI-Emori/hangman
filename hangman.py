f766db7bebea5d39536dd632f13cbf1840514456
KOJI-Emori <gu0enn_galaxy@yahoo.co.jp>
2019-02-20 00:31:39 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,36 @@
+Python 3.8.0a1 (tags/v3.8.0a1:e75eeb00b5, Feb  3 2019, 20:47:39) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+
+
+def hangman():
+    word_list = ["Python", "Java", "computer", "hacker", "painter"]
+    random_number = random.randint(0, 4)
+    word = word_list[random_number]
+    wrong_guesses = 0
+    stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+    remaining_letters = list(word)
+    letter_board = ["__"] * len(word)
+    win = False
+    print('Welcome to Hangman')
+    while wrong_guesses < len(stages) - 1:
+        print('\n')
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+        if '__' not in letter_board:
+            print('You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0: wrong_guesses]))
+        print('You lose! The words was {}'.format(word))
+
+hangman()
